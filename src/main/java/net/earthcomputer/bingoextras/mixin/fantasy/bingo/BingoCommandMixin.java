0ad5eded50bd54bfa137ee28b3e43908c053aa96
joe <burtonjae@hotmail.co.uk>
2024-05-02 20:37:13 +0100

    Get player teams from the command context

--- src/main/java/net/earthcomputer/bingoextras/mixin/fantasy/bingo/BingoCommandMixin.java
+++ src/main/java/net/earthcomputer/bingoextras/mixin/fantasy/bingo/BingoCommandMixin.java
@@ -1,11 +1,11 @@
 package net.earthcomputer.bingoextras.mixin.fantasy.bingo;
 
+import com.mojang.brigadier.context.CommandContext;
 import net.earthcomputer.bingoextras.FantasyUtil;
+import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.world.scores.PlayerTeam;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -13,13 +13,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(targets = "io.github.gaming32.bingo.BingoCommand", remap = false)
 @Pseudo
 public class BingoCommandMixin {
-    @Shadow
-    @Final
-    private PlayerTeam[] teams;
-
     @Inject(method = "resetGame", at = @At("RETURN"))
-    private void onEndGame(CallbackInfo ci) {
-        for (PlayerTeam team : teams) {
+    private void onEndGame(CommandContext<CommandSourceStack> context, CallbackInfo ci) {
+        for (PlayerTeam team : context.getSource().getServer().getScoreboard().getPlayerTeams()) {
             FantasyUtil.destroyTeamSpecificLevels(team);
         }
     }
