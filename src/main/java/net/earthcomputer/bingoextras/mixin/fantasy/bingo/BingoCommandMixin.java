d06c32c57b344689a08b9459f7af5ab2a001cefe
joe <burtonjae@hotmail.co.uk>
2024-05-02 20:39:02 +0100

    Use CallbackInfoReturnable

--- src/main/java/net/earthcomputer/bingoextras/mixin/fantasy/bingo/BingoCommandMixin.java
+++ src/main/java/net/earthcomputer/bingoextras/mixin/fantasy/bingo/BingoCommandMixin.java
@@ -8,13 +8,13 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(targets = "io.github.gaming32.bingo.BingoCommand", remap = false)
 @Pseudo
 public class BingoCommandMixin {
     @Inject(method = "resetGame", at = @At("RETURN"))
-    private static void onResetGame(CommandContext<CommandSourceStack> context, CallbackInfo ci) {
+    private static void onResetGame(CommandContext<CommandSourceStack> context, CallbackInfoReturnable<Integer> cir) {
         for (PlayerTeam team : context.getSource().getServer().getScoreboard().getPlayerTeams()) {
             FantasyUtil.destroyTeamSpecificLevels(team);
         }
