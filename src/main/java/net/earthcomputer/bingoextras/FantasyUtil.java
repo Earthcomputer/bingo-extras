c55335f2634908c75fcb4487ce264a3397ac74eb
joe <burtonjae@hotmail.co.uk>
2024-10-07 15:04:28 +0100

    Remove unused import

--- src/main/java/net/earthcomputer/bingoextras/FantasyUtil.java
+++ src/main/java/net/earthcomputer/bingoextras/FantasyUtil.java
@@ -12,7 +12,6 @@ import org.jetbrains.annotations.Nullable;
 import xyz.nucleoid.fantasy.RuntimeWorldHandle;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 import java.util.function.Supplier;
 
