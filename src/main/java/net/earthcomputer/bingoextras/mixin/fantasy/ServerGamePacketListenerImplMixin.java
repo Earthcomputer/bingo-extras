1f07081b8103cd4c10caecb85b245313c25e6fda
joe <burtonjae@hotmail.co.uk>
2024-10-23 22:49:32 +0100

    Fix ServerGamePacketListenerImplMixin

--- src/main/java/net/earthcomputer/bingoextras/mixin/fantasy/ServerGamePacketListenerImplMixin.java
+++ src/main/java/net/earthcomputer/bingoextras/mixin/fantasy/ServerGamePacketListenerImplMixin.java
@@ -6,13 +6,17 @@ import net.earthcomputer.bingoextras.FantasyUtil;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
+import net.minecraft.world.entity.Relative;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
+import java.util.Set;
+
 @Mixin(ServerGamePacketListenerImpl.class)
 public class ServerGamePacketListenerImplMixin {
-    @WrapOperation(method = "handleTeleportToEntityPacket", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerPlayer;teleportTo(Lnet/minecraft/server/level/ServerLevel;DDDFF)V"))
-    private void forceTeleport(ServerPlayer instance, ServerLevel serverLevel, double d, double e, double f, float g, float h, Operation<Void> original) {
-        FantasyUtil.forceDimensionChange(() -> original.call(instance, serverLevel, d, e, f, g, h));
+    @WrapOperation(method = "handleTeleportToEntityPacket", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerPlayer;teleportTo(Lnet/minecraft/server/level/ServerLevel;DDDLjava/util/Set;FFZ)Z"))
+    private boolean forceTeleport(ServerPlayer instance, ServerLevel serverLevel, double d, double e, double f, Set<Relative> set, float g, float h, boolean bl, Operation<Boolean> original) {
+        FantasyUtil.forceDimensionChange(() -> original.call(instance, serverLevel, d, e, f, set, g, h, bl));
+        return bl;
     }
 }
